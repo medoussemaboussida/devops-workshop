f4a9808d42a1281df9cce9f04557b7bad8643717
med oussema boussida <medoussemaboussida@gmail.com>
2025-02-13 12:57:44 +0100

    first commit in gitbash way

---
+++ src/main/java/com/esprit/microservice/gatewayapi/GatewayApiApplication.java
@@ -0,0 +1,26 @@
+package com.esprit.microservice.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("candidat", r -> r.path("/candidats/**")
+						.uri("http://localhost:8082/")) // Load Balancer vers le microservice "MS-CANDIDAT"
+				.route("Job", r -> r.path("/jobs/**")
+						.uri("http://localhost:8081/")) // Load Balancer vers le microservice "MS-JOB"
+				.build();
+	}
+}
